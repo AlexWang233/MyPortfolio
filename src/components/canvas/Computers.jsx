1b9815b0711daf1cfd60de8ad47f0e6243c2fca2
AlexWang233 <alex13901350@gmail.com>
2023-05-28 23:00:37 -0400

    Three.js Init

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-
+import React from "react";
+import { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import CanvasLoader from "../Loader";
 const Computers = () => {
-  return (
-    <div>Computers</div>
-  )
-}
+  return <div>Computers</div>;
+};
 
-export default Computers
+export default Computers;
