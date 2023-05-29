4d8fe8090f5cd9dc6725c3a98ee987140d50d0c8
AlexWang233 <alex13901350@gmail.com>
2023-05-28 20:57:56 -0400

    Navbar

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,17 @@
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
   ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react", "react-refresh"],
   rules: {
-    'react-refresh/only-export-components': 'warn',
+    "react/jsx-uses-vars": "error",
+    "react/jsx-uses-react": "error",
+    "react-refresh/only-export-components": "warn",
   },
-}
+};
