07a4d830cfc6afe64980fc58b90a5fc412b7b697
AlexWang233 <alex13901350@gmail.com>
2023-05-29 21:53:17 -0400

    Update Ball.jsx

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import {
+  Decal,
+  Float,
+  OrbitControls,
+  Preload,
+  useTexture,
+} from "@react-three/drei";
+import { CanvasLoader } from "../Loader";
 
-const Ball = () => {
+const Ball = (props) => {
+  const [decal] = useTexture([props.imgUrl]);
+  return <div>Ball</div>;
+};
+
+const BallCanvas = ({ icon }) => {
   return (
-    <div>Ball</div>
-  )
-}
+    <Canvas frameLoop="demand" gl={{ preserveDrawingBuffer: true }}>
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false} />
+        <Ball imgUrl={icon} />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+};
 
-export default Ball
+export default BallCanvas;
