6ab1bf9761bc4173e947207267ff326b80f15293
AlexWang233 <alex13901350@gmail.com>
2023-05-29 22:34:56 -0400

    Update Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { github } from "../assets";
+import { SectionWrapper } from "../hoc";
+import { projects } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const Works = () => {
   return (
-    <div>Works</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>My Achievements</p>
+        <h2 className={styles.sectionHeadText}>Work Experiences</h2>
+      </motion.div>
+    </>
+  );
+};
 
-export default Works
+export default Works;
