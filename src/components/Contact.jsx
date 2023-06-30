881e62098dd4a973c5032f2883fb1457374d11b8
AlexWang233 <alex13901350@gmail.com>
2023-06-29 22:57:45 -0400

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,7 @@ import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
 const Contact = () => {
+  const formRef = useRef();
   const [form, setForm] = useState({
     name: "",
     email: "",
