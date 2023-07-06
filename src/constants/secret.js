5460f8b7f5f80d4bc7e83eebdb95db304f163857
AlexWang233 <alex13901350@gmail.com>
2023-07-05 23:06:12 -0400

    EmailJS Account

---
+++ src/constants/secret.js
@@ -0,0 +1,8 @@
+const emailjsConfig = {
+    SERVICE_ID: 'service_g24hs3a',
+    TEMPLATE_ID : 'template_mfqclkd',
+    PUBLIC_KEY : 'DRxWuxJIb3Tn0RjWA',
+    MY_EMAIL: 'alex13901350@gmail.com'
+}
+
+export {emailjsConfig}
